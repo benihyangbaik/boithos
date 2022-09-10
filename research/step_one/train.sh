1cb5aad97711ae93cb1ed75acf66fd3a4d61b1bb
Aranggi J. Toar <at@aranggitoar.com>
2022-09-11 04:35:44 +0700

    Logic: Added script for training the model on CPU.

--- research/step_one/train.sh
+++ research/step_one/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p checkpoints/boithos
-
 python -m sockeye.train \
-  --prepared-data train-data-bin \
+  --prepared-data train-data \
   --validation-source data/bible.prep/valid.src \
   --validation-target data/bible.prep/valid.tgt \
+  --use-cpu \
   --output checkpoints/boithos \
+  --shared-vocab \
   --max-num-epochs 3
