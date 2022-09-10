a415fc93cc4779657191b4177ef444599a716987
Aranggi J. Toar <at@aranggitoar.com>
2022-09-10 14:26:23 +0700

    Idem

--- research/step_one/prepare4outsource.sh
+++ research/step_one/prepare4outsource.sh
@@ -2,18 +2,22 @@
 
 mkdir -p outsource
 mkdir -p outsource/valid-data
-mkdir -p outsource/test-data
+# Unecessary for now
+# mkdir -p outsource/test-data
 mkdir -p outsource/trans-raw
 
 # Zip validation data for training and testing data for scoring
-tar -czf outsource/valid-data/src.gz data/bible.prep/valid.src
-tar -czf outsource/valid-data/tgt.gz data/bible.prep/valid.tgt
-tar -czf outsource/test-data/src.gz data/bible.prep/test.src
-tar -czf outsource/test-data/tgt.gz data/bible.prep/test.tgt
+# Some are unecessary for now
+# tar -czf outsource/test-data/src.gz data/bible.prep/test.src
+# tar -czf outsource/test-data/tgt.gz data/bible.prep/test.tgt
+tar -czf outsource/valid-data/src.gz data/bible.prep/tmp/valid.src
+tar -czf outsource/valid-data/tgt.gz data/bible.prep/tmp/valid.tgt
 
 # Copy training data
-cp -r train-data ./outsource
-cp data/bible.prep/code ./outsource
+cp -r train-data outsource
 
 # Copy translation template
-cp data/bible.prep/src-template ./outsource/trans-raw
+cp data/bible.prep/src-template outsource/trans-raw
+
+# Copy the Jupyter Notebook script
+cp step_one.ipynb outsource
